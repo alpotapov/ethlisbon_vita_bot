cdb48cfae3cd8d1825a05cf805076211fcdccd35
Alexander Potapov <al.potapov@hotmail.com>
2021-10-25 23:19:27 +0200

    Initial

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const RINKEBY_ACCOUNT_1 = '84566c8a1763d634e65bbf94efdcfe220128c1818b3a205617b03504c53a073c';
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +23,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.RINKEBY_INFURA_KEY}`,
+      accounts: [`0x${RINKEBY_ACCOUNT_1}`]
+    },
+  },
 };
